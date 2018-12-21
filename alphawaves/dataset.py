4546703e4377bca971309b12ef88d007163d0ec2
Pedro Rodrigues <pedro.rodrigues01@gmail.com>
2018-12-21 17:24:34 +0100

    fixing some useless loops

--- alphawaves/dataset.py
+++ alphawaves/dataset.py
@@ -135,41 +135,39 @@ class AlphaWaves():
     def _get_single_subject_data(self, subject):
         """return data for a single subject"""
 
-        file_path_list = self.data_path(subject)
-        for filepath in file_path_list:
+        filepath = file_path_list[0]:
+        data = loadmat(filepath)
 
-            data = loadmat(filepath)
+        S = data['SIGNAL'][:, 1:17]
+        stim_close = data['SIGNAL'][:, 17]
+        stim_open = data['SIGNAL'][:, 18]
+        stim = 1 * stim_close + 2 * stim_open
 
-            S = data['SIGNAL'][:, 1:17]
-            stim_close = data['SIGNAL'][:, 17]
-            stim_open = data['SIGNAL'][:, 18]
-            stim = 1 * stim_close + 2 * stim_open
+        chnames = [
+            'Fp1',
+            'Fp2',
+            'Fc5',
+            'Fz',
+            'Fc6',
+            'T7',
+            'Cz',
+            'T8',
+            'P7',
+            'P3',
+            'Pz',
+            'P4',
+            'P8',
+            'O1',
+            'Oz',
+            'O2',
+            'stim']
+        chtypes = ['eeg'] * 16 + ['stim']
+        X = np.concatenate([S, stim[:, None]], axis=1).T
 
-            chnames = [
-                'Fp1',
-                'Fp2',
-                'Fc5',
-                'Fz',
-                'Fc6',
-                'T7',
-                'Cz',
-                'T8',
-                'P7',
-                'P3',
-                'Pz',
-                'P4',
-                'P8',
-                'O1',
-                'Oz',
-                'O2',
-                'stim']
-            chtypes = ['eeg'] * 16 + ['stim']
-            X = np.concatenate([S, stim[:, None]], axis=1).T
-
-            info = mne.create_info(ch_names=chnames, sfreq=512,
-                                   ch_types=chtypes, montage='standard_1020',
-                                   verbose=False)
-            raw = mne.io.RawArray(data=X, info=info, verbose=False)
+        info = mne.create_info(ch_names=chnames, sfreq=512,
+                               ch_types=chtypes, montage='standard_1020',
+                               verbose=False)
+        raw = mne.io.RawArray(data=X, info=info, verbose=False)
 
         return raw
 
