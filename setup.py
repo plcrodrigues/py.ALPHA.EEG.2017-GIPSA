b8bfa038840dbe95f34265994558394f36ea9238
gcattan <gcattan@hotmail.fr>
2022-07-05 21:33:13 +0200

    change install requires order

--- setup.py
+++ setup.py
@@ -135,14 +135,14 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    setup_requires=["numpy>=1.13.3"],
+    # setup_requires=["numpy>=1.13.3"],
     install_requires=[
             "mne==0.16.1",
             "pyriemann==0.2.6",
             "setuptools==40.6.3",
+            "scikit_learn==0.23",
             "scipy==1.7.2",
-            "matplotlib==3.4.3",
-            "scikit_learn==0.23"],
+            "matplotlib==3.4.3"],
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
     # syntax, for example:
