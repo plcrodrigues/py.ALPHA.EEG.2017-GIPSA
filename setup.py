b70e715d781321a6309f9c03bcb6cd0c8e8ee72d
gcattan <gcattan@hotmail.fr>
2023-01-02 19:32:52 +0100

    Fix MNE _fetch_file (#15)
    * Update download.py
    
    Remove mne._fetch_file as per https://github.com/NeuroTechX/moabb/pull/188/files#diff-e4d2cd05ed4b1394f44ae809ae7f290ac3c42021dccf0ecb5ba9f5614dec55b0R25
    
    * Update requirements.txt
    
    * Update setup.py
    
    * Update download.py
    
    * Update download.py
    
    * Update download.py
    
    * Update requirements.txt
    
    * Update setup.py
    
    * Update dataset.py

--- setup.py
+++ setup.py
@@ -137,11 +137,12 @@ setup(
         "setuptools>=40.6.3",
         "numpy>=1.13.3"],
     install_requires=[
-            "mne==0.16.1",
+            "mne==0.24.1",
             "pyriemann>=0.2.6",
             "scipy>=1.8",
             "scikit_learn>=0.23",
-            "matplotlib>=3.4.3"],
+            "matplotlib>=3.4.3",
+            "pooch==1.6.0"],
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
     # syntax, for example:
