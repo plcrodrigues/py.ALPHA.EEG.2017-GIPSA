1341afbf83cb42f693baa70a833a8dd41ce427d5
gcattan <gcattan@hotmail.fr>
2024-01-04 14:17:49 +0100

    Update setup.py
    upgrade version number

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.4',  # Required
+    version='0.5',  # Required
 
     license='new BSD',
 
